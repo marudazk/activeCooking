3c4d257132c5b645481c5c4ae7258c95c2f35e0f
Aneta Znyk <marudazk@gmail.com>
2025-08-07 14:32:49 +0200

    server initialized

---
+++ server/src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("🚀 Backend działa!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.get('/recipes', (req, res) => {
+  res.json([
+    { id: 1, title: 'Makaron z pesto' },
+    { id: 2, title: 'Pizza Margherita' }
+  ]);
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server działa na http://localhost:${PORT}`);
+});
